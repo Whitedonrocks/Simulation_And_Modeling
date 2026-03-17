11de837ffd4349c68d20858fa0b46721f92a9e08
Whitedonrocks <prayagnepal2060@gmail.com>
2026-03-17 08:36:17 +0545

    Lab 1

--- Lab 1/qn1.c
+++ Lab 1/qn1.c
@@ -5,86 +5,71 @@
 // c) Repeat the experiment for at least five different values of input iterations and tabulate the true value, the experiment's output, and the error percentage for each.
 
 
+/*
+ Algorithm:
+ 1. Initialize circle_points and square_points, interval to zero
+ 2. generate random point 'x' (beteeen 0 and 1)
+ 3. generate random point 'y (betweeen 0 and 1)
+ 4. calculate d = x^2 + y^2
+ 5. if d <= 1, increment circle_point.
+ 6. increment interval, increment square point
+ 7. if increment < No. of interations, repeat from step 2.
+ 8. calculate, pi = 4 * (circle_points / square_points) 
+ 9. Display the value
+*/
 
-//  Algorithm:
-//  1. Initialize circle_points and square_points, interval to zero
-//  2. generate random point 'x' (beteeen 0 and 1)
-//  3. generate random point 'y (betweeen 0 and 1)
-//  4. calculate d = x^2 + y^2
-//  5. if d <= 1, increment circle_point.
-//  6. increment interval, increment square point
-//  7. if increment < No. of interations, repeat from step 2.
-//  8. calculate, pi = 4 * (circle_points / square_points) 
-//  9. Display the value
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
 
 #define SEED 35791246
-#define TRUE_PI 3.14159265358979323846
 
-int main()
-{
-    int iterations = 0, circle_points = 0, square_points = 0;
-    double pi, z, x, y;
-    int i = 0;
+int main() {
+    int n, i, j;
+    int iterations[100];
+    double pi[100], error[100];
+    double trueValueOfPI = 3.141592;
 
-    printf("Enter the number of iterations to estimate PI: ");
-    scanf("%d", &iterations);
+    printf("Enter number of different iteration inputs: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        printf("Enter iteration value %d: ", i+1);
+        scanf("%d", &iterations[i]);
+    }
 
     srand(SEED);
 
-    for (i = 0; i < iterations; i++)
-    {
-        x = (double)rand() / RAND_MAX;
-        y = (double)rand() / RAND_MAX;
-        z = x * x + y * y;
-        if (z <= 1) circle_points++;
-        square_points++;
-    }
 
-    pi = 4 * (double)circle_points / (double)square_points;
-    printf("\nEstimated value of PI : %.8lf\n", pi);
-    printf("True value of PI      : %.8lf\n", TRUE_PI);
-    printf("Error percentage      : %.6lf%%\n", fabs((pi - TRUE_PI) / TRUE_PI) * 100.0);
+    for(i = 0; i < n; i++) {
+        int square_point = 0, circle_point = 0;
+        double x, y, z;
 
-    int testCases[] = {100, 1000, 10000, 100000, 1000000, 10000000};
-    int n = sizeof(testCases) / sizeof(testCases[0]);
-
-    printf("\n--- Part (c): Experiment Across Multiple Iterations ---\n\n");
-    printf("%-15s %-18s %-18s %-12s\n",
-           "Iterations", "True Value (PI)", "Estimated PI", "Error (%)");
-    printf("%s\n", "---------------------------------------------------------------");
-
-    for (int t = 0; t < n; t++)
-    {
-        circle_points = 0;
-        square_points = 0;
-
-        srand(SEED); 
-
-        for (i = 0; i < testCases[t]; i++)
-        {
+        for(j = 0; j < iterations[i]; j++) {
             x = (double)rand() / RAND_MAX;
             y = (double)rand() / RAND_MAX;
-            z = x * x + y * y;
-            if (z <= 1) circle_points++;
-            square_points++;
+
+            z = x*x + y*y;
+
+            if(z <= 1)
+                circle_point++;
+
+            square_point++;
         }
 
-        pi = 4 * (double)circle_points / (double)square_points;
-        double error = fabs((pi - TRUE_PI) / TRUE_PI) * 100.0;
-
-        printf("%-15d %-18.8lf %-18.8lf %-12.6lf\n",
-               testCases[t], TRUE_PI, pi, error);
+        pi[i] = 4 * ((double)circle_point / (double)square_point);
+        error[i] = fabs((trueValueOfPI - pi[i]) / trueValueOfPI) * 100;
     }
 
-    printf("\nObservation: As iterations increase by 10x,\n");
-    printf("error decreases by ~sqrt(10) due to O(1/sqrt(N)) convergence.\n");
+    printf("\n");
+    printf("Iterations\tTrue PI\t\tEstimated PI\tError Percentage\n");
+    printf("\n");
+
+    for(i = 0; i < n; i++) {
+        printf("%d\t\t%lf\t%lf\t%.2lf\n",
+               iterations[i], trueValueOfPI, pi[i], error[i]);
+    }
 
     return 0;
 }
-
